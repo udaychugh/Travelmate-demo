66630a5085a6f37acd83920f3de94e9af135c753
Uday Chugh <61309187+udaychugh@users.noreply.github.com>
2022-01-08 16:57:27 +0530

    Add files via upload

---
+++ demo/script/script.js
@@ -0,0 +1,11 @@
+function start(){
+    location.replace("first-form.html");
+}
+
+function next1(){
+    location.replace("second-form.html");
+}
+
+function next2(){
+    location.replace("journey.html");
+}
